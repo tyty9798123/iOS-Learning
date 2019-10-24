3a58b60ae7eea8aca1ac46263dc1287ebdf81737
cspltw <tyty979812@gmail.com>
2019-10-24 18:34:57 +0800

    UI course 1~4

--- PbUICourse/ViewController.swift
+++ PbUICourse/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var btn1: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("View controller view Did Load")
+        
+        label1.text = "Progress Bar iOS Swift"
+        btn1.setTitle("系統確認中...請稍等", for: .disabled)
     }
 
+    @IBAction func btn1Clicked(_ sender: UIButton) {
+        print("Clicked1")
+        btn1.isEnabled = false
 
+    }
+    
 }
 
