5e0db82f092fe9ef6801a4ba470d74e549336ecd
cspltw <tyty979812@gmail.com>
2019-11-18 14:08:10 +0800

    Stack View

---
+++ PbUICourse/LayoutTestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LayoutTestViewController.swift
+//  PbUICourse
+//
+//  Created by 劉謹賢 on 2019/11/18.
+//  Copyright © 2019 劉謹賢. All rights reserved.
+//
+
+import UIKit
+
+class LayoutTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
